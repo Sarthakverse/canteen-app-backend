56f2b9d9074e919f842ed838626312314c9e0a13
aakarsh09Z <singhaakarsh54321@gmail.com>
2023-12-14 20:13:18 +0530

    added cors access

--- src/main/java/com/example/jwtauthorisedlogin/controller/AdminController.java
+++ src/main/java/com/example/jwtauthorisedlogin/controller/AdminController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
--- src/main/java/com/example/jwtauthorisedlogin/controller/FoodController.java
+++ src/main/java/com/example/jwtauthorisedlogin/controller/FoodController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
